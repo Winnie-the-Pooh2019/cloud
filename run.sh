09546785be05c036f5887c8f2475818bdbb6c0b6
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2025-03-04 19:10:27 +0700

    dfdsf

--- run.sh
+++ run.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-docker compose -f ./compose-setup.yaml up -d nginx
-docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email -d "cloud.duvanoff.su" -d "www.cloud.duvanoff.su" --preferred-challenges http
-docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email -d "office.duvanoff.su" -d "www.office.duvanoff.su" --preferred-challenges http
-docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email -d "duvanoff.su" -d "www.duvanoff.su" --preferred-challenges http
-docker compose -f ./compose-setup.yaml down nginx
-
-docker compose -f ./compose.yaml up -d
-
-docker compose -f ./compose-setup.yaml run --rm php-config
+docker-compose run --volume $1:/tmp/source backup-service python backup.py
