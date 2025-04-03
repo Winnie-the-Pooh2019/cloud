d8bfac0526a696409c03e1107683158e631af95c
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2025-04-03 17:58:30 +0700

    working on backups

---
+++ backup.sh
@@ -0,0 +1,5 @@
+docker compose -f ./compose-setup.yaml run --build \
+-v $(pwd)/volumes/source:/tmp/dist \
+-v $(pwd)/volumes/backup/:/tmp/back \
+backup \
+-a backup
--- backup/main.py
+++ backup/main.py
@@ -1,13 +1,16 @@
 import os
+import time
 import shutil
 import hashlib
 import argparse
 import zipfile
+import re
+import glob
+from datetime import datetime
 
 backup_filename = 'backup'
 backup_extension = 'zip'
 checksum_filename = 'checksum.sha256'
-
 
 class RestoreException(Exception):
     pass
@@ -17,8 +20,36 @@ class CopyException(Exception):
     pass
 
 
-class BackupException(Exception):
+class NothingToBackupException(Exception):
     pass
+
+
+class ChecksumIsActualException(Exception):
+    pass
+
+
+class BackupActualException(Exception):
+    pass
+
+
+class NeedBackupException(Exception):
+    pass
+
+
+def read_file(file_path):
+    if not os.path.exists(file_path):
+        return None
+    
+    with open(file_path, 'r') as file:
+        return file.read()
+
+
+def get_creation_date(file_path):
+    try:
+        creation_time = os.path.getctime(file_path)
+        return time.ctime(creation_time)
+    except OSError:
+        return None
 
 
 def get_checksum(directory):
@@ -53,6 +84,10 @@ def restore(destination, backup):
     os.remove(destination_archive)
 
 
+def backup(destination, backup):
+    pass
+
+
 def copy_data(source, destination):
     if not os.path.exists(source):
         raise FileNotFoundError('Source directory does not exist.')
@@ -75,14 +110,31 @@ bkp = args.backup
 dist = args.destination
 
 backup_file = f'{bkp}/{backup_filename}.{backup_extension}'
+checksum_filepath_bkp = f'{bkp}/{checksum_filename}'
+checksum_filepath_dist = f'{dist}/{checksum_filename}'
+backup_filepath = f'{bkp}/{backup_filename}.{backup_extension}'
 
 if action == 'check':
     try:
         try:
             if os.listdir(dist):
-                if os.path.exists(backup_file):
+
+                checksum_content = read_file(checksum_filepath_bkp)
+                checksum_dist = read_file(checksum_filepath_dist)
+
+                if os.path.exists(backup_filepath) and checksum_content is not None and checksum_dist is not None and checksum_content == checksum_dist:
+                    raise BackupActualException
+
+                if  checksum_content != checksum_dist:
+                    if get_creation_date(checksum_filepath_bkp) < get_creation_date(checksum_filepath_dist):
+                        raise NeedBackupException
+                    else:
+                        raise RestoreException
+
+
+
                     try:
-                        with zipfile.ZipFile(backup_file) as z:
+                        with zipfile.ZipFile(backup_filepath) as z:
                             with z.open(checksum_filename, 'r') as checksum_file:
                                 backup_checksum = checksum_file.read()
                     except (zipfile.BadZipFile, FileNotFoundError) as e:
@@ -95,21 +147,23 @@ if action == 'check':
                         dist_checksum = '-1'
 
                     if backup_checksum != '-1' and backup_checksum != dist_checksum:
-                        raise RestoreException()
+                        raise RestoreException
                 else:
                     if not os.listdir(dist):
                         raise CopyException
             else:
-                if os.path.exists(backup_file):
+                if os.path.exists(backup_filepath):
                     raise RestoreException
                 else:
                     raise CopyException
         except RestoreException:
             print('Data is not actual... Restoring...')
             restore(dist, bkp)
-        except CopyException:
-            print('No data and no backup files found... Restoring...')
-            copy_data(src, dist)
+        except NeedBackupException:
+            print('Data in backup is not actual... Backuping')
+            backup(dist, bkp)
+    except BackupActualException as e:
+        print('Backup is actual... Nothing to do')
     except FileNotFoundError as e:
         print(f'{e}')
     except Exception as e:
@@ -117,17 +171,22 @@ if action == 'check':
 elif action == 'backup':
     try:
         if not os.listdir(dist):
-            raise BackupException
+            raise NothingToBackupException
 
         content_hash = get_checksum(dist)
 
-        with open(f'{dist}/{checksum_filename}', "w") as checksum_file:
+        if content_hash is not None and content_hash == read_file(checksum_filepath_bkp):
+            raise ChecksumIsActualException
+
+        with open(checksum_filepath_bkp, "w") as checksum_file:
             checksum_file.write(content_hash)
 
-        archived = shutil.make_archive(f'{bkp}/{backup_filename}', backup_extension, dist)
+        archived = shutil.make_archive(backup_filepath, backup_extension, dist)
 
         print('Backup completed')
-    except BackupException:
+    except ChecksumIsActualException:
+        print('Checksum is actual... Nothing to backup')
+    except NothingToBackupException:
         print('Backup failed. Nothing to backup')
     except Exception as e:
         print('Unexpected error:', e)
