f19f753e89fb7945ad06a9aee892b0854981afa1
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2025-09-03 09:29:13 +0700

    fixed certificates renewing

---
+++ cert-renew.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker compose -f "$1"/compose.yaml stop
+
+docker compose -f "$1"/compose-setup.yaml up -d nginx-setup
+docker compose -f "$1"/compose-setup.yaml run --rm certbot renew
+docker compose -f "$1"/compose-setup.yaml down nginx-setup
+
+docker compose -f "$1"/compose.yaml start
--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,15 @@
 #!/bin/bash
 
-#volumes=( \
-#"db-data" \
-#"cloud-data" \
-#"db-office-data" \
-#"office-data" \
-#"office-public" \
-#"office-fonts" \
-#"track-data" \
-#)
-#
-#for volume in "${volumes[@]}"; do
-#  echo "Restoring volume: $volume"
-#
-#  docker compose -f ./compose.yaml run --build \
-#  -v "cloud_$volume":/tmp/dist \
-#  -v $(pwd)/backup/"$volume"/:/tmp/back --rm backup -a check
-#
-#  docker run --rm -v "cloud_$volume":/volume alpine chown -R 1000:1000 /volume
-#  docker run --rm -v "cloud_$volume":/volume alpine chmod -R 755 /volume
-#
-#done
-
 echo "Setting up certificates"
-docker compose -f ./compose-setup.yaml up -d nginx
+docker compose -f ./compose-setup.yaml up -d nginx-setup
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "cloud.duvanoff.su" -d "www.cloud.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "office.duvanoff.su" -d "www.office.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "track.duvanoff.su" -d "www.track.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "duvanoff.su" -d "www.duvanoff.su" --preferred-challenges http
-docker compose -f ./compose-setup.yaml down nginx
+docker compose -f ./compose-setup.yaml down nginx-setup
 
 echo "Initialize services"
 docker compose -f ./compose-setup.yaml run --rm php-config
-#docker compose -f ./compose.yaml up -d
-#echo "Sleeping for 60 seconds"
-#sleep 60
-#docker compose -f ./compose.yaml stop
-#
-#echo "Restoring volumes from backup"
-#docker compose -f ./compose.yaml run --rm --build backup -c restore
-#echo "Restoring complete"
 
 echo "Run services"
 docker compose -f ./compose.yaml up -d
@@ -47,7 +17,7 @@ docker compose -f ./compose.yaml up -d
 curdir=$(pwd)
 
 echo "adding tasks to cron"
-echo "0 9 * * 1 /usr/bin/docker compose -f $curdir/compose-setup.yaml run --rm certbot renew >> /var/log/renew-ssl.log && /usr/bin/docker compose -f $(pwd)/compose.yaml kill -s SIGHUP nginx" > ./cron
-#echo "30 7 * * * /usr/bin/docker compose -f $curdir/compose.yaml run --rm --build backup -a check >> /var/log/backup.log" >> ./cron
+echo "0 9 * * 1 $curdir/cert-renew.sh $curdir >> /var/log/renew-ssl.log" > ./cron-file
 
-crontab ./cron
+crontab ./cron-file
+rm ./cron-file
