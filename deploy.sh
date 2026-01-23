056119d6708474aa6e166c08cf060fe80c09861d
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2026-01-23 18:21:39 +0700

    added taigaio support

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Setting up certificates"
 docker compose -f ./compose-setup.yaml up -d nginx-setup
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "cloud.duvanoff.su" -d "www.cloud.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "office.duvanoff.su" -d "www.office.duvanoff.su" --preferred-challenges http
-docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "track.duvanoff.su" -d "www.track.duvanoff.su" --preferred-challenges http
+docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "taiga.duvanoff.su" -d "www.taiga.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "duvanoff.su" -d "www.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml down nginx-setup
 
@@ -13,6 +13,7 @@ docker compose -f ./compose-setup.yaml run --rm php-config
 
 echo "Run services"
 docker compose -f ./compose.yaml up -d
+docker compose -f ./taigaio.yaml up -d
 
 curdir=$(pwd)
 
