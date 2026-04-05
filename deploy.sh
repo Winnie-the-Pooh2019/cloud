f65b94198630c1fb8a1c96cadc327082547d4ad4
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2026-04-05 12:08:20 +0700

    feat: added docmost support

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "Stopping containers"
+docker compose -f ./compose.yaml stop
+
 echo "Setting up certificates"
 docker compose -f ./compose-setup.yaml up -d nginx-setup
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "cloud.duvanoff.su" -d "www.cloud.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "office.duvanoff.su" -d "www.office.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "taiga.duvanoff.su" -d "www.taiga.duvanoff.su" --preferred-challenges http
+docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "doc.duvanoff.su" -d "www.doc.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email "ivan.duvanov.3@gmail.com" --agree-tos --no-eff-email --non-interactive --keep-until-expiring -d "duvanoff.su" -d "www.duvanoff.su" --preferred-challenges http
 docker compose -f ./compose-setup.yaml down nginx-setup
 
@@ -12,8 +16,9 @@ echo "Initialize services"
 docker compose -f ./compose-setup.yaml run --rm php-config
 
 echo "Run services"
-docker compose -f ./compose.yaml up -d
 docker compose -f ./taigaio.yaml up -d
+docker compose -f ./docmost.yaml up -d
+docker compose -f ./compose.yaml up -d
 
 curdir=$(pwd)
 
