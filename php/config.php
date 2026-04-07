d1cb313e5c5cce5da91299da469e51f68421c6fe
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2026-04-07 07:26:57 +0700

    fix: nextcloud config patched

--- php/config.php
+++ php/config.php
@@ -1,5 +1,10 @@
-'trusted_domains' => array (
-  0 => 'cloud.duvanoff.su',
-),
-'overwrite.cli.url' => 'https://cloud.duvanoff.su',
-'overwriteprotocol' => 'https',
+<?php
+$CONFIG = array(
+  'dbhost'              => 'cloud-db',
+  'trusted_domains'     => array(
+    0 => 'cloud.duvanoff.su',
+  ),
+  'overwrite.cli.url'   => 'https://cloud.duvanoff.su',
+  'overwriteprotocol'   => 'https',
+  'overwritehost'       => 'cloud.duvanoff.su',
+);
